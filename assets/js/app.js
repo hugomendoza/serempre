03d7179a5c2b74813c068407d70a0d53d7920299
hugomendoza <cronoshimura@yahoo.es>
2020-03-15 19:01:55 -0500

    versión final

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  var counterPack = 0;
+  var counterNumber = 09530;
+
+  $('.counterPack').html(counterPack);
+  $('.counterPrice').html(counterPack);
+  $('.counter__number span').html(counterNumber);
+
+  //Incrementa el valor
+
+  $('.more').click(function() {
+    if(counterPack < 2) {
+      counterPack++;
+      counterNumber--;
+      $('.counterPack').html(counterPack);
+      $('.counterPrice').html(counterPack);
+      $('.counter__number span').html(counterNumber);
+    }
+  });
+
+  //Disminuye le valor
+
+  $('.less').click(function() {
+    if(counterPack > 0) {
+      counterPack--;
+      counterNumber++;
+      $('.counterPack').html(counterPack);
+      $('.counterPrice').html(counterPack);
+      $('.counter__number span').html(counterNumber);
+    }
+  });
+
+  $('.order').click(function () { 
+    if(counterPack == 0) {
+      alert('Parcero, debes pedir al menos 1 pack');
+    }
+    else {
+      $('.pre-order').hide();
+      $('.success').fadeIn();
+    }
+  });
+});
